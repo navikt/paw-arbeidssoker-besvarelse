6254fccd448cbfa14bfa91521af0afcb9fcbc0f9
Jostein Holje <jostein.holje@nav.no>
2023-05-08 12:29:53 +0200

    legg til idporten

--- src/main/kotlin/no/nav/paw/besvarelse/routes/ApiRoutes.kt
+++ src/main/kotlin/no/nav/paw/besvarelse/routes/ApiRoutes.kt
@@ -19,7 +19,7 @@ import org.koin.ktor.ext.inject
 fun Route.apiRoutes() {
     val arbeidssokerRegistrertService: ArbeidssokerRegistrertService by inject()
 
-    authenticate("tokendings") {
+    authenticate("tokendings", "idporten") {
         route("/api/v1") {
             get("/besvarelse") {
                 logger.info("Henter siste besvarelse til bruker")
